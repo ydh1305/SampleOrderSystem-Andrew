488fa3bddf0ac965d315383241b30cef2e56bf5a
donghyun.yoon <ydh1305@naver.com>
2026-06-12 17:39:11 +0900

    [fix] encoding: /utf-8 컴파일러 플래그 및 콘솔 UTF-8 코드페이지 설정으로 한글 깨짐 수정

--- SampleOrderSystem/main.cpp
+++ SampleOrderSystem/main.cpp
@@ -1,7 +1,10 @@
 #include "controller/MainController.h"
 #include "view/ConsoleUtil.h"
+#include <windows.h>
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
     ConsoleUtil::enableVirtualTerminal();
     MainController().run();
     return 0;
